948f6cb49eccb1098b8a4ba9306830f178900a1f
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-22 17:52:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -14,13 +14,3 @@ function removeFromPlaylist(playlist,artistName)
 }
 
 
-  describe('removeFromPlaylist(playlist, artistName)', () => {
-    it('removes `artistName` from `playlist`', () => {
-      removeFromPlaylist(playlist, 'Slowdive')
-
-      expect(playlist).
-        to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-      expect(playlist).
-        not.to.have.all.keys({'Slowdive': 'Alison'})
-    })
