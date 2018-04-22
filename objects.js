6f7fc70cdbafa0b52c9dc35b839183d0c50da94b
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-22 17:51:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,10 +4,14 @@ var playlist = {2Pac:"All eyez on me"};
 function updatePlaylist(playlist,artistName,songTitle)
 {
     Object.assign(playlist,{artistName:songTitle});
+    //playlist["artistName"]=songTitle;
 }
 
 
-
+function removeFromPlaylist(playlist,artistName)
+{
+  
+}
 
 
   describe('removeFromPlaylist(playlist, artistName)', () => {
